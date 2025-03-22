174a09d1654e777d6ab88a440d1652a49919ba01
amadeus-b <167602238+amadeus-b@users.noreply.github.com>
2025-03-22 18:08:50 +0100

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,12 +4,11 @@ import styles from './styles.module.css';
 
 const FeatureList = [
 {
-  title: 'Blog rund um Thai Massage',
+  title: 'Thai-Massage Verzeichnis',
   Webp: require('@site/static/img/verzeichnis.webp').default,
   description: (
     <>
-      Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-      ahead and move your docs into the <code>docs</code> directory.
+      Finden Sie Thai-Massage Studios. Auf Thai-Massage.de sind mehr als 4.000 Studios aus ganz Deutschland gelistet.
       <div style={{ marginTop: '1rem' }}>
         <a
           className="button button--secondary button--lg"
@@ -26,7 +25,7 @@ const FeatureList = [
 
 function Feature({Webp, title, description}) {
   return (
-    <div className={clsx('col col--12')}>
+    <div className={clsx('col col--6')}>
       <div className="text--center">
         <img src={Webp} className={styles.featureWebp} alt={title} />
       </div>
