553f43838e797c18efdbd367887179c21d045a2d
amadeus-b <167602238+amadeus-b@users.noreply.github.com>
2025-03-22 16:00:11 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,8 +39,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Blog rund um das Thema Traditionelle Thai-Massage">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
