49205a5a500d464d945e96c4ce554a608a070e97
amadeus-b <167602238+amadeus-b@users.noreply.github.com>
2025-03-22 18:17:49 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -142,7 +142,7 @@ const config = {
             ],
           },
         ],*/
-        copyright: `Copyright © ${new Date().getFullYear()} Thai-Massage.de - Blog`.  <a href="https://thai-massage.de/impressum" target="_blank" rel="noopener noreferrer">Impressum</a>`,
+        copyright: `Copyright © ${new Date().getFullYear()} Thai-Massage.de - Blog`,
         
       },
       prism: {
