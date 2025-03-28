fc13a41bca89d0c09e730d244427c87cc0c142ab
amadeus-b <167602238+amadeus-b@users.noreply.github.com>
2025-03-28 20:32:09 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,6 +80,7 @@ const config = {
       require.resolve("@easyops-cn/docusaurus-search-local"),
       /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
       ({
+        indexDocs: true,
         // ... Your options.
         // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
